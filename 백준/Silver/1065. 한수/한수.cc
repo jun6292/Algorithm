7c8701f34f0cc7d1861cc751a3b40ca6664d53f6
WonJun <c68254@gmail.com>
2022-05-28 00:12:20 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int arithmetic_seq(int n)
+{
+  if (n < 100)
+    return (1);
+
+  int n1, n2, n3;
+  n1 = n % 10;
+  n2 = (n % 100) / 10; 
+  n3 = n / 100;
+  if ((n3 - n2) == (n2 - n1))
+    return (1);
+  return (0);
+}
+
+int main(void)
+{
+  int cnt = 0;
+  int N;
+  cin >> N;
+  for(int i = 1; i <= N; i++){
+    if(arithmetic_seq(i))
+      cnt++;
+  }
+  cout << cnt << '\n';
+  return 0;
+}
