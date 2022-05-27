521a3c532f47542c44d3bfbb75c7d5d872225a22
WonJun <c68254@gmail.com>
2022-05-27 16:41:52 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N, j;
+    string str;
+    int cnt = 0;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> str;
+        int arr[26] = { 0 };
+        arr[str[0] - 'a'] = 1;
+        for(j = 1; j < str.length(); j++){
+            if (str[j - 1] != str[j]){
+                if(arr[str[j] - 'a'] == 0)
+                    arr[str[j] - 'a']++;
+                else
+                    break;
+            }
+        }
+        if (j == str.length())
+            cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
