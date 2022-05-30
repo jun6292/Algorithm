46043d5c8b16ecb42f1aae644c1d9e763d34b42e
WonJun Jo <c68254@gmail.com>
2022-05-30 13:34:42 +0900

    [level 2] Title: 피보나치 수, Time: 0.51 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int Fibonacci[100001];
+    Fibonacci[0] = 0;
+    Fibonacci[1] = 1;
+    for (int i = 2; i <= n; i++)
+        Fibonacci[i] = (Fibonacci[i - 1] + Fibonacci[i - 2]) % 1234567;
+    answer = Fibonacci[n];
+    return answer;
+}
