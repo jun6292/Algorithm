107ecafc56cf736ded267a8de86a8a009a39c0e0
WonJun Jo <c68254@gmail.com>
2022-06-20 00:52:12 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int pcnt = 0, ycnt = 0;
+    for (int i = 0; i < s.length(); i++){
+        if (s[i] == 'p' || s[i] == 'P')
+            pcnt++;
+        else if (s[i] == 'y' || s[i] == 'Y')
+            ycnt++;
+    }
+    if (pcnt == ycnt)
+        answer = true;
+    else
+        answer = false;
+    return answer;
+}
