f7e9cdee341b9a679fa4ea5014277f905d5371ec
WonJun Jo <c68254@gmail.com>
2022-08-28 00:50:14 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> computers) {
+    int net = 0;
+    vector<bool> visited(n, false);
+    
+    for (int i = 0; i < n; i++) {
+        
+        if (visited[i])
+            continue;
+        
+        queue<int> q;
+        q.push(i);
+        
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            
+            if (visited[v])
+                continue;
+            
+            visited[v] = true;
+            
+            for (int j = 0; j < n; j++) {
+                if (v != j && computers[v][j] == 1 && !visited[j])
+                    q.push(j);
+            }
+        }
+        
+        net++;
+    }
+    
+    return net;
+}
