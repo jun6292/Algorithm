c280fbfc0e458694924228484f8a89a89ec7dcb5
WonJun Jo <c68254@gmail.com>
2022-10-16 00:05:38 +0900

    [Bronze I] Title: 단어 공부, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int alpha[26] = { 0 };
+  int cnt = 0;
+  string S;
+  cin >> S;
+
+  for(int i = 0; i < S.length(); i++){
+    if (S[i] >= 'a' && S[i] <= 'z')
+      S[i] -= 32;
+    alpha[S[i] - 'A']++;
+  }
+  int max = alpha[0];
+  int max_idx = 0;
+  for(int i = 0; i < 26; i++){
+    if (max < alpha[i]){
+      max = alpha[i];
+      max_idx = i;
+    }
+  }
+  for(int i = 0; i < 26; i++){
+    if (max == alpha[i])
+      cnt++;
+  }
+  if (cnt > 1)
+    cout << "?";
+  else
+    cout << char(max_idx + 65);
+  return 0;
+}
