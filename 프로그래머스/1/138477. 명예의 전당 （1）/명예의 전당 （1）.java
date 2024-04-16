dc722f7f10a868e7d7520d3a18fbb6c46ad6cb67
WonJun <c68254@gmail.com>
2024-04-17 01:04:54 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> honor = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            honor.add(score[i]);
+            if (honor.size() > k) {
+                honor.remove(Collections.min(honor));
+            }
+            answer[i] = Collections.min(honor);
+        }
+        return answer;
+    }
+    
+}
