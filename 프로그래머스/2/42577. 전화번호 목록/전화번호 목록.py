477c09b1aa01153f22ac285b08957b8f6cd8471d
WonJun <c68254@gmail.com>
2024-07-26 14:07:33 +0900

    [level 2] Title: 전화번호 목록, Time: 100.13 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -5,4 +5,16 @@ def solution(phone_book):
         if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
             answer = False
     return answer
-        
+
+# def solution(phone_book):
+#     phone_dict = dict()
+#     for phone_number in phone_book:
+#         phone_dict[phone_number] = 1
+    
+#     for phone_number in phone_book:
+#         prefix = ""
+#         for number in phone_number:
+#             prefix += number
+#             if prefix in phone_dict.keys() and prefix != phone_number:
+#                 return False
+#     return True
