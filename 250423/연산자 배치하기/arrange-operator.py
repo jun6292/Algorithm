f20e22749fa4de6c4c940b124f1771b8af5f3876
Wonjun <c68254@gmail.com>
2025-04-23 16:51:00 +0900

    Title: 연산자 배치하기, Time: 55ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250423/연산자 배치하기/arrange-operator.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/14888
+# https://www.codetree.ai/ko/frequent-problems/problems/arrange-operator/description
+# 백준 14888 연산자 끼워넣기
+# 코드트리 연산자 배치하기
+
+n = int(input())
+numbers = list(map(int, input().split()))
+calc = list(map(int, input().split()))
+max_value, min_value = -1e9, 1e9
+
+visited = [False] * n
+def dfs(num, depth):
+    global max_value, min_value
+
+    if depth == n:
+        max_value, min_value = max(max_value, num), min(min_value, num)
+        return
+
+    for i in range(3):
+        if calc[i] != 0:
+            calc[i] -= 1
+            if i == 0:
+                result = num + numbers[depth]
+            elif i == 1:
+                result = num - numbers[depth]
+            elif i == 2:
+                result = num * numbers[depth]
+            dfs(result, depth + 1)
+            calc[i] += 1
+
+dfs(numbers[0], 1)
+
+print(min_value, max_value)
